f6e52057c929d7a9d53cd08a695d93b62c769460
LTH <johtta888@naver.com>
2023-01-01 02:43:55 +0900

    Feat: 추가 로그인 상태면 user menu render

--- src/components/@base/SvgIcon/icons.ts
+++ src/components/@base/SvgIcon/icons.ts
@@ -1,1 +1,2 @@
 export { default as naver } from "assets/icons/naver.svg";
+export { default as user } from "assets/icons/user.svg";
--- src/components/@module/Header/index.tsx
+++ src/components/@module/Header/index.tsx
@@ -1,12 +1,15 @@
 import { Logotypo } from "components/@base";
 import { LoginButton } from "components/Auth";
+import { UserMenu } from "components/User";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 function Header() {
+  const { status } = useSession();
   return (
     <header className="flex p-4 items-center justify-between">
       <Logotypo />
-      <LoginButton />
+      {status === "authenticated" ? <UserMenu /> : <LoginButton />}
     </header>
   );
 }
