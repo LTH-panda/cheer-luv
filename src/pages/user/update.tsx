1eae25af55a0cf2c0be21448ed7a0ad60f74a564
LTH <johtta888@naver.com>
2023-01-01 22:07:36 +0900

    Feat: 추가 닉네임 수정

--- src/pages/user/update.tsx
+++ src/pages/user/update.tsx
@@ -1,10 +1,15 @@
-import { Layout } from "components/@module";
+import { ActionsHeader, Layout } from "components/@module";
+import { UserProfileUpdateForm } from "components/User";
 import React from "react";
 
 function ProfileUpdatePage() {
   return (
     <Layout>
-      <div>updatepage</div>
+      <ActionsHeader />
+      <main className="p-4 flex flex-col gap-4">
+        <h1 className="text-2xl">프로필 수정</h1>
+        <UserProfileUpdateForm callback="/user" />
+      </main>
     </Layout>
   );
 }
